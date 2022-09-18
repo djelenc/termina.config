d787955107e10e607394bf0749470c5984ea77fe
David Jelenc <david.jelenc@fri.uni-lj.si>
2022-09-18 13:41:08 +0200

    Removes unused configs, updates ssh

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ set +e
 DATE=`date '+%Y-%m-%d-%H%M%S'`
 
 # vimrc
-mv ~/.vimrc ~/.vimrc-$DATE && echo "Backing up existing vimrc to ~/.vimrc-$DATE" || echo "No prior vimrc"
-ln -sf $PWD/vimrc ~/.vimrc
-mv ~/.vimrc.local ~/.vimrc.local-$DATE && echo "Backing up existing vimrc.local to ~/.vimrc.local-$DATE" || echo "No prior vimrc.local"
-ln -sf $PWD/vimrc.local ~/.vimrc.local
-mv ~/.vimrc.local.bundles ~/.vimrc.local.bundles-$DATE && echo "Backing up existing vimrc.local.bundles to ~/.vimrc.local.bundles-$DATE" || echo "No prior vimrc.local.bundles"
-ln -sf $PWD/vimrc.local.bundles ~/.vimrc.local.bundles
+# mv ~/.vimrc ~/.vimrc-$DATE && echo "Backing up existing vimrc to ~/.vimrc-$DATE" || echo "No prior vimrc"
+# ln -sf $PWD/vimrc ~/.vimrc
+# mv ~/.vimrc.local ~/.vimrc.local-$DATE && echo "Backing up existing vimrc.local to ~/.vimrc.local-$DATE" || echo "No prior vimrc.local"
+# ln -sf $PWD/vimrc.local ~/.vimrc.local
+# mv ~/.vimrc.local.bundles ~/.vimrc.local.bundles-$DATE && echo "Backing up existing vimrc.local.bundles to ~/.vimrc.local.bundles-$DATE" || echo "No prior vimrc.local.bundles"
+# ln -sf $PWD/vimrc.local.bundles ~/.vimrc.local.bundles
 
 # tmux
 mv ~/.tmux.conf ~/.tmux.conf-$DATE && echo "Backing up existing tmux.conf to ~/.tmux.conf-$DATE" || echo "No prior tmux.conf"
@@ -21,12 +21,12 @@ mv ~/.ssh/config ~/.ssh/config-$DATE && echo "Backing up existing ssh/config to 
 ln -sf $PWD/ssh.config ~/.ssh/config
 
 # zshrc
-mv ~/.zshrc ~/.zshrc-$DATE && echo "Backing up existing zshrc ~/.zshrc-$DATE" || echo "No prior zshrc"
-ln -sf $PWD/zshrc ~/.zshrc
+# mv ~/.zshrc ~/.zshrc-$DATE && echo "Backing up existing zshrc ~/.zshrc-$DATE" || echo "No prior zshrc"
+# ln -sf $PWD/zshrc ~/.zshrc
 
 # spacemacs
-mv ~/.spacemacs ~/.spacemacs-$DATE && echo "Backing up existing spacemacs to ~/.spacemacs-$DATE" || echo "No prior spacemacs"
-ln -sf $PWD/spacemacs ~/.spacemacs
+# mv ~/.spacemacs ~/.spacemacs-$DATE && echo "Backing up existing spacemacs to ~/.spacemacs-$DATE" || echo "No prior spacemacs"
+# ln -sf $PWD/spacemacs ~/.spacemacs
 
 # ideavimrc
 mv ~/.ideavimrc ~/.ideavimrc-$DATE && echo "Backing up existing ideavimrc ~/.ideavimrc-$DATE" || echo "No prior ideavimrc"
